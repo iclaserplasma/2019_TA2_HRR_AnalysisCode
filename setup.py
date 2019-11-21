c9ae1807a43d2505a0d1e2ee002a4612927d6dd0
iclaserplasma <>
2019-11-21 12:56:17 +0000

    Added setup.py to make it installable via pip.
    Also changed file structure and added .gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TA2_HRR_AnalysisCode',
+      version='0.1',
+      description='Library of analysis code for the 2019 TA2 HRR experiment',
+      author='Rob Shalloo, Matthew Streeter, Nic Gruse',
+      packages=['ta2_hrr_analysisCode', ],
+      long_description=open('README.md').read(),
+     )
