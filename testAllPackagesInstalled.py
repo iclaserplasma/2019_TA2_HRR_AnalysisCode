890ef8eb0f9185b6f0cac38752a6eb94ea7c6bff
Christopher Underwood <christopher.underwood@york.ac.uk>
2020-02-25 12:56:37 +0000

    Updating list of requirements, and a script to run to check that all the modules are availiable. Due to the error catching in dataRun, it is hard to locate when the packages are not installed

---
+++ testAllPackagesInstalled.py
@@ -0,0 +1,13 @@
+import numpy
+import scipy
+import skimage
+import sklearn
+import pandas
+import shapely
+import abel
+import mysql
+import cv2 
+import peakutils
+import peakdetect
+
+print ("No import errors, Good to go")
