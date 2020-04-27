95159880060f91675b3fdea232c3476edb31592e
Rob Shalloo <rob.shalloo@gmail.com>
2020-04-27 22:51:15 +0100

    added function to retrieve spectral phase profile

--- ta2_hrr_analysisCode/SPIDERAnalysis.py
+++ ta2_hrr_analysisCode/SPIDERAnalysis.py
@@ -46,7 +46,7 @@ def readSPIDER_spectral_profile_FULL(path):
             for i, line in enumerate(z.open(filename)):
                  if not i == 0:
                      # The data is in binary, so we need to decode it to append to arrays
-                     d.append([float(line.split(b"\t")[1].decode()), float(line.split(b"\t")[2].decode())]),float(line.split(b"\t")[3].decode())])
+                     d.append([ float(line.split(b"\t")[1].decode()), float(line.split(b"\t")[2].decode()),float(line.split(b"\t")[3].decode())])
             d = np.array(d)
             return d[:,0], d[:,1], d[:,2]
     # If it hasn't found the data return arrays of zero
